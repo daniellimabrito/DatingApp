4c87c99fb8a8905d5231903639ab9a6fbceb1ae6
Daniel Brito <danielbrito@Daniels-MacBook-Pro.local>
2019-12-15 19:14:28 -0800

    Initial Commit

---
+++ DatingApp.API/Controllers/Models/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values {get;set;}
+
+    }
+}
